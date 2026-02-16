8480c7e6d8198e87070c9145918ad57ec9a1f5d9
ayath <ayath.ayath8@etu.univ-lorraine.fr>
2026-02-16 13:47:45 +0100

    traiter les litteraux

---
+++ Util/Litteral/traiterLitteraux.py
@@ -0,0 +1,34 @@
+from Util.TermStore.TermList import TermSystem
+from Algos.MartelliMontanari import MartelliMontanari, UnificationError
+
+def traiter_litteraux(liste_litteraux):
+
+    for i in range(len(liste_litteraux)):
+        for j in range(i+1, len(liste_litteraux)):
+
+            l1 = liste_litteraux[i]
+            l2 = liste_litteraux[j]
+
+            if l1.predicat == l2.predicat and l1.sign != l2.sign:
+                if l1.arity != l2.arity:
+                    print(f"Échec : Arités différentes pour {l1} et {l2}")
+                    continue
+
+                system = TermSystem()
+
+                for t1, t2 in zip(l1.enfants, l2.enfants):
+                    system.add(t1, t2)
+
+                mm = MartelliMontanari(system)
+
+                try:
+                    unificateur=mm.solve()
+                    print("Unification réussie")
+                    if unificateur.is_empty():
+                     print("   (Système vide : les termes étaient identiques)")
+                    else:
+                        for eq in unificateur.equations:
+                            print(f"   {eq.left} --> {eq.right}")
+                except UnificationError:
+                    print("Échec")
+    print("Fin du traitement.")
