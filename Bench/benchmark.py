518267797fb02f4d265b22bf651229bcaf36d4f5
Matheo <matheo.vigneres@gmail.com>
2026-04-21 17:02:37 +0200

    ajout MM au bench

--- Bench/benchmark.py
+++ Bench/benchmark.py
@@ -5,13 +5,14 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")
 
 from Algos.ArbreDiscrimination.arbre_de_discrimination import benchmark_arbre_discrimination
 from Algos.Robinson.benchmark import benchRobinson
+from Algos.MartelliMontanari.bench import bench
 
 from Util.Serialisation.serialisation import deserialiser
 from Util.Litteral.Litteral import Litteral
 
 def benchmark(candidat: str, filename: str, algo: str, structure: str):
     touteUnif = True
-    pretraitement = True
+    pretraitement = False
     if algo == "arbre":
         print("Début déserialisation")
         predList = deserialiser(os.path.basename(filename), False)
@@ -19,12 +20,19 @@ def benchmark(candidat: str, filename: str, algo: str, structure: str):
         print("Debut algo")
         tps_pretraitement, tps_unif = benchmark_arbre_discrimination(predList, realCandidat, touteUnif)
         print(tps_pretraitement, tps_unif)
-    if algo == "robinson":
+    elif algo == "robinson":
         print("Début déserialisation")
         predList = deserialiser(os.path.basename(filename), False)
         realCandidat = Litteral.from_string(candidat)
         print("Debut algo")
         tps_pretraitement, tps_unif = benchRobinson(realCandidat, predList, structure, pretraitement, touteUnif)
+        print(tps_pretraitement, tps_unif)
+    elif algo == "mm":
+        print("Début déserialisation")
+        predList = deserialiser(os.path.basename(filename), False)
+        realCandidat = Litteral.from_string(candidat)
+        print("Debut algo")
+        tps_pretraitement, tps_unif = bench(realCandidat, predList, structure, pretraitement, touteUnif)
         print(tps_pretraitement, tps_unif)
 
 
@@ -38,4 +46,4 @@ if __name__ == "__main__":
         "Output",
         filename,
     )
-    benchmark("Q(X, f(Y), Z, g(X))", file, "robinson", "ensemble")
+    benchmark("Q(X, f(Y), Z, g(X))", file, "mm", "liste")
