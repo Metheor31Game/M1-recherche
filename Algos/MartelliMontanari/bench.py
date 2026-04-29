7e3d040b7dc30578c3de26636ae134a3a4b3e930
ayath <ayath.ayath8@etu.univ-lorraine.fr>
2026-04-29 11:21:25 +0200

    modif du bench

--- Algos/MartelliMontanari/bench.py
+++ Algos/MartelliMontanari/bench.py
@@ -6,46 +6,27 @@ from Util.Litteral.traiterLitterauxSet import lit_Set, traiterlitterauxSet
 from Algos.MartelliMontanari.MartelliMontanari import MartelliMontanari, UnificationError
 from Util.TermStore.TermList import TermSystem
 
-def bench(candidat: Litteral, predList: list, structure: str, pretraitement: bool, touteUnif=True):
+def bench(candidats: list, predList: list, structure: str, pretraitement: bool, touteUnif=True):
     
     tps_pretraitement = 0.0
     collection = predList  
+    resultats = []
 
     if pretraitement:
         debut = time.perf_counter()
 
         if structure == "liste":
-            # On filtre la liste pour avoir seulement les candidats éligibles pour l'unification
-            collection = []
-            for lit in predList:
-                if (
-                    lit.predicat == candidat.predicat
-                    and lit.sign != candidat.sign
-                    and lit.arity == candidat.arity
-                ):
-                    collection.append(lit)
+            # On ne pourra pas filtrer la liste car on travaille sur plusieurs candidat
+            collection = predList
 
         elif structure == "ensemble":
-            # On transforme la liste en set avec filtrage
-            collection = set()
-            for lit in predList:
-                if (
-                    lit.predicat == candidat.predicat
-                    and lit.sign != candidat.sign
-                    and lit.arity == candidat.arity
-                ):
-                    collection.add(lit)
+            # On transforme la liste en set 
+            collection = set(predList)
 
         elif structure == "dictionnaire":
-            # On transforme la liste en index 
-            index = indexer(predList)
-            if candidat.predicat in index:
-                if candidat.sign:
-                    collection = index[candidat.predicat]["negatifs"]
-                else:
-                    collection = index[candidat.predicat]["positifs"]
-            else:
-                collection = []
+            # On transforme la liste en index
+            collection = indexer(predList)
+            
 
         tps_pretraitement = time.perf_counter() - debut
 
@@ -55,42 +36,47 @@ def bench(candidat: Litteral, predList: list, structure: str, pretraitement: boo
             collection = set(predList)
 
         elif structure == "dictionnaire":
-            index = indexer(predList)
-            if candidat.predicat in index:
+            collection = indexer(predList)
+        
+    for candidat in candidats:
+        debut = time.perf_counter() #Ici on mesure le temps d'unification pour chaque candidat
+        resultat = {}
+
+        sousCollection = collection
+        if structure == "dictionnaire":
+            if candidat.predicat in collection:
                 if candidat.sign:
-                    collection = index[candidat.predicat]["negatifs"]
+                    sousCollection = collection[candidat.predicat]["negatif"]
                 else:
-                    collection = index[candidat.predicat]["positifs"]
+                    sousCollection = collection[candidat.predicat]["positif"]
             else:
-                collection = []
+                sousCollection = []  
 
-    debut = time.perf_counter() #Ici on mesure le temps d'unification 
-    resultat = {}
+        for l2 in sousCollection:
+            # Si il y a pas de prétraitement, le filtre se fait ici pendant l'unification
+            if structure != "dictionnaire":
+                if l2.predicat != candidat.predicat or l2.sign == candidat.sign or l2.arity != candidat.arity:
+                    continue
 
-    for l2 in collection:
-        # Si il y a pas de prétraitement, le filtre se fait ici pendant l'unification
-        if not pretraitement:
-            if l2.predicat != candidat.predicat or l2.sign == candidat.sign or l2.arity != candidat.arity:
-                continue
+            system = TermSystem()
+            for t1, t2 in zip(candidat.enfants, l2.enfants):
+                system.add(t1, t2)
 
-        system = TermSystem()
-        for t1, t2 in zip(candidat.enfants, l2.enfants):
-            system.add(t1, t2)
+            mm = MartelliMontanari(system)
+            try:
+                resultat[l2] = mm.solve()
+                if not touteUnif:
+                    break  # on s'arrête après la première unification réussie
+            except UnificationError:
+                pass
 
-        mm = MartelliMontanari(system)
-        try:
-            resultat[l2] = mm.solve()
-            if not touteUnif:
-                break  # on s'arrête après la première unification réussie
-        except UnificationError:
-            pass
+        tps_unification = time.perf_counter() - debut
 
-    tps_unification = time.perf_counter() - debut
+        resultats.append(( tps_unification, len(resultat)))
 
    
     print(f"Structure           : {structure}")
     print(f"Temps prétraitement : {tps_pretraitement} s")
-    print(f"Temps unification   : {tps_unification} s")
-    print(f"Le nombre d'unifications : {len(resultat)}")
+   
 
-    return (tps_pretraitement, tps_unification, len(resultat))
+    return (tps_pretraitement, resultats)
