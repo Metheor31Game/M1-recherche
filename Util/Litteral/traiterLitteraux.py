65af01c42db0e1b3a72652cb49da78d34f13cde7
ayath <ayath.ayath8@etu.univ-lorraine.fr>
2026-05-02 06:42:26 +0200

    modif

--- Util/Litteral/traiterLitteraux.py
+++ Util/Litteral/traiterLitteraux.py
@@ -1,5 +1,6 @@
 from Util.TermStore.TermList import TermSystem
 from Algos.MartelliMontanari.MartelliMontanari import MartelliMontanari, UnificationError
+from Util.Litteral.Litteral import Litteral
 
 def traiter_litteraux(liste_litteraux):
 
@@ -47,16 +48,16 @@ def traiter_litteraux(liste_litteraux):
     return comparaisons, succes, echec
 
 
-def lit_liste(l1,list_litteraux, touteUnif=True):
+def lit_liste(l1: Litteral, list_litteraux: list[Litteral], touteUnif=True):
     resultat = {}
-    for l2 in list_litteraux:
-        if l1.predicat == l2.predicat and l1.sign != l2.sign and l1.arity == l2.arity:
-            system = TermSystem()
+    for l2 in list_litteraux: # Etape 1 : On compare le littéral l1 avec tous les autres littéraux de la liste
+        if l1.predicat == l2.predicat and l1.sign != l2.sign and l1.arity == l2.arity: #Etape 2 : On vérifie si les deux prédicats ont les mêmes symboles, les signes opposés et les arités égales
+            system = TermSystem() 
 
             for t1, t2 in zip(l1.enfants, l2.enfants):
-                system.add(t1, t2)
+                system.add(t1, t2) # Etape 3 : Si les conditions sont remplies, on crée un système d'équations à partir des enfants des deux littéraux
 
-            mm = MartelliMontanari(system)
+            mm = MartelliMontanari(system) # Etape 4 : On utilise l'algorithme de Martelli-Montanari pour tenter de résoudre le système d'équations
             try:
                 unificateur=mm.solve()                  
                 resultat[l2] = unificateur
